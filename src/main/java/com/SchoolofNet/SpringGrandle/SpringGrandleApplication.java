54849a838714fc14c0e8813f84798c4c5fba49a7
Douglas Teixeira Rodrigues <rm78770@fiap.com.br>
2019-11-09 03:16:29 -0300

    Commit 01

---
+++ src/main/java/com/SchoolofNet/SpringGrandle/SpringGrandleApplication.java
@@ -0,0 +1,13 @@
+package com.SchoolofNet.SpringGrandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrandleApplication.class, args);
+	}
+
+}
